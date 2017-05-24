67f7f9bfc166ad4b1ec60eb443e312b47d38ed95
Yuwei Sun <sunyuwei@cloudin.cn>
2017-05-24 10:47:27 +0800

    使用 Vue 重构'参考'部分内容

--- js/koshien99.js
+++ js/koshien99.js
@@ -1,6 +1,30 @@
-var app = new Vue({
+var title = new Vue({
   el: '#site-title',
   data: {
     title: '第99回全国高等学校野球選手権大会'
+  }
+});
+
+
+var reference = new Vue({
+  el: '#reference',
+  data: {
+    links: [
+      {
+        content: 'NHK Online',
+        url: 'http://www.nhk.or.jp/koushien',
+        img: 'img/nhk_logo.png'
+      },
+      {
+        content: 'MBS',
+        url: 'http://mainichi.jp/koshien/',
+        img: 'img/mbs_logo_s.png'
+      },
+      {
+        content: '朝日新聞',
+        url: 'http://www.asahi.com/koshien',
+        img: 'img/asahi_logo.png'
+      }
+    ]
   }
 });
